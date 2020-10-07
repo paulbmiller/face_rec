c4f0f77192d321a85c465602ce9a33080eac493b
Paul Miller <miller_p@hotmail.fr>
2020-10-07 14:49:59 +0200

    Working version for distinguishing both faces

---
+++ README.md
@@ -0,0 +1,10 @@
+# Face Recognition
+## Description
+
+Small face recognition implementation which uses the *face_recognition* library to recognize people in a video based on photos of them.
+
+Runs a bit slow and haven't gotten around to improving it, but it does work in distinguishing between Obama and Key's faces.
+
+## Example
+
+![Sample](example.png)
--- video_face_rec.py
+++ video_face_rec.py
@@ -2,8 +2,9 @@ import face_recognition
 import os
 import cv2
 import pickle
-import time
-from PIL import ImageGrab
+import dlib
+
+dlib.DLIB_USE_CUDA = True
 
 # https:\\\\www.youtube.com\\watch?v=PdkPI92KSIs
 
@@ -11,40 +12,54 @@ KNOWN_FACES_DIR = "known_faces"
 TOLERANCE = 0.5
 FRAME_THICKNESS = 3
 FONT_THICKNESS = 2
-MODEL = "cnn"
+MODEL = "hog"
 
 video = cv2.VideoCapture('obamaspeech.mp4')
-video.set(cv2.CAP_PROP_BUFFERSIZE, 2)
 
-print("loading known faces")
+"""
+video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+video.set(cv2.CAP_PROP_BUFFERSIZE, 2)
+"""
+
+print("Loading known faces")
 
 known_faces = []
 known_names = []
 
 for name in os.listdir(KNOWN_FACES_DIR):
     for filename in os.listdir(f"{KNOWN_FACES_DIR}\\{name}"):
-        # image = face_recognition.load_image_file(
-        #     f"{KNOWN_FACES_DIR}\\{name}\\{filename}")
-        # encoding = face_recognition.face_encodings(image)[0]
+        image = face_recognition.load_image_file(
+            f"{KNOWN_FACES_DIR}\\{name}\\{filename}")
+        encoding = face_recognition.face_encodings(image)[0]
         
-        encoding = pickle.load(open(f"{name}\\{filename}", "rb"))
         known_faces.append(encoding)
-        known_names.append(int(name))
+        known_names.append(name)
 
-
-if len(known_names) > 0:
-    next_id = max(known_names) + 1
+nb_known = len(os.listdir(KNOWN_FACES_DIR))
+if nb_known > 0:
+    next_id = nb_known
 else:
     next_id = 0
 
+start_frame_number = 650
+video.set(cv2.CAP_PROP_POS_FRAMES, start_frame_number)
 
-print("processing unknown faces")
-while video.isOpened():
-    ret, image = video.read()
+
+print("Processing unknown faces")
+while True:
+    RATIO = 1
+    ret, original = video.read()
+    
+    original = original[:-80, 160:]
+    
+    image = cv2.resize(original, (original.shape[1] // RATIO,
+                                  original.shape[0] // RATIO))
 
     # image = face_recognition.load_image_file()
+    locations = face_recognition.face_locations(image,
+                                                number_of_times_to_upsample=1,
+                                                model=MODEL)
 
-    locations = face_recognition.face_locations(image, model=MODEL)
     encodings = face_recognition.face_encodings(image, locations)
     # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
@@ -60,26 +75,31 @@ while video.isOpened():
             next_id += 1
             known_names.append(match)
             known_faces.append(face_encoding)
+            """
             os.mkdir(f"{KNOWN_FACES_DIR}\\{match}")
-            file = open(f"{KNOWN_FACES_DIR}\\{match}\\{match}-{int(time.time())}.pkl",
-                        "wb")
+            file = open(
+                f"{KNOWN_FACES_DIR}\\{match}\\{match}-{int(time.time())}.pkl",
+                "wb")
             pickle.dump(face_encoding, file)
+            """
 
-        top_left = (face_location[3], face_location[0])
-        bottom_right = (face_location[1], face_location[2])
+        top_left = (face_location[3] * RATIO, face_location[0] * RATIO)
+        bottom_right = (face_location[1] * RATIO, face_location[2] * RATIO)
         color = [0, 255, 0]
-        cv2.rectangle(image, top_left, bottom_right, color,
+        cv2.rectangle(original, top_left, bottom_right, color,
                       FRAME_THICKNESS)
 
-        top_left = (face_location[3], face_location[2])
-        bottom_right = (face_location[1], face_location[2] + 22)
-        cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
-        cv2.putText(image, str(match),
-                    (face_location[3] + 10, face_location[2] + 15),
+        top_left = (face_location[3] * RATIO, face_location[2] * RATIO)
+        bottom_right = (face_location[1] * RATIO,
+                        face_location[2] * RATIO + 22)
+        cv2.rectangle(original, top_left, bottom_right, color, cv2.FILLED)
+        cv2.putText(original, str(match),
+                    (face_location[3] * RATIO + 10,
+                     face_location[2] * RATIO + 15),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200),
                     FONT_THICKNESS)
 
-    cv2.imshow("", image)
+    cv2.imshow("", original)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
